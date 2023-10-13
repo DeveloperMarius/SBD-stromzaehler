f6597aa28b5fa8b55c61cf75209ffbc5dbd743a5
Marius Karstedt <info@marius-karstedt.de>
2023-10-13 12:30:17 +0200

    added working demo with jwt verification for selected routes

--- smart-meter-stromzaehler/app.py
+++ smart-meter-stromzaehler/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import os
 from dotenv import load_dotenv
+from auth_middleware import token_required
 
 load_dotenv()
 
@@ -11,6 +12,37 @@ JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 print(JWT_SECRET_KEY)
 app.config['SECRET_KEY'] = JWT_SECRET_KEY
 
+
 @app.route("/api")
 def hello_world():
     return "Hello, World!"
+
+
+@app.route("/protected", methods=["GET"])
+@token_required
+def get_current_user():
+    # request.json
+    return jsonify({
+        "message": "successfully retrieved user profile",
+        "data": "a"
+    })
+
+@app.errorhandler(403)
+def forbidden(e):
+    return jsonify({
+        "message": "Forbidden",
+        "error": str(e),
+        "data": None
+    }), 403
+
+@app.errorhandler(404)
+def forbidden(e):
+    return jsonify({
+        "message": "Endpoint Not Found",
+        "error": str(e),
+        "data": None
+    }), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
