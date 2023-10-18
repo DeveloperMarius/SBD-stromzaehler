af46579344915d7f883046cc6fdc5e4e75d3eb8e
jakscha <mails-straeucher0l@icloud.com>
2023-10-18 18:59:59 +0200

    Fixed Path

--- smart-meter-stromzaehler/src/database.py
+++ smart-meter-stromzaehler/src/database.py
@@ -1,8 +1,9 @@
 import sqlite3
+import os
 
 
 class Database:
 
     def __init__(self):
-        self.connection = sqlite3.connect('../res/stromzaehler.db', check_same_thread=False, isolation_level=None)
+        self.connection = sqlite3.connect(f"{os.path.dirname(os.path.realpath(__file__))}/../res/stromzaehler.db", check_same_thread=False, isolation_level=None)
         self.cursor = self.connection.cursor()
