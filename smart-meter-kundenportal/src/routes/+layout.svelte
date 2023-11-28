cd1e18765838cb838862a32dab06270a71072190
sebastian@hauschildt.dev <sebastian@hauschildt.dev>
2023-11-28 16:15:56 +0100

    Userdaten ändern funktioniert

--- smart-meter-kundenportal/src/routes/+layout.svelte
+++ smart-meter-kundenportal/src/routes/+layout.svelte
@@ -2,7 +2,11 @@
 	import '../app.css';
 	import '@fontsource/roboto';
 	import '@fontsource/manrope';
+
+	import { Toaster } from 'svelte-french-toast';
 </script>
+
+<Toaster />
 
 <slot><!-- optional fallback --></slot>
 
--- smart-meter-kundenportal/src/routes/dashboard/profile/+page.server.ts
+++ smart-meter-kundenportal/src/routes/dashboard/profile/+page.server.ts
@@ -1,3 +1,161 @@
-import { auth_guard } from '$lib/auth';
+import jwt from 'jsonwebtoken';
+import { auth_guard, type AuthGuardOutput } from '$lib/auth';
+import prisma from '$lib/prisma';
+import { redirect, type Actions, type ServerLoad, fail } from '@sveltejs/kit';
+import { z } from 'zod';
+import type { Prisma } from '@prisma/client';
 
-export const load = auth_guard;
+export const load: ServerLoad = async (event) => {
+	const auth = auth_guard(event) as AuthGuardOutput;
+	const user = auth?.data?.user;
+
+	if (!user) {
+		throw redirect(302, `/auth/login`);
+	}
+
+	const userData = await prisma.user.findFirst({
+		where: {
+			id: user.id
+		}
+	});
+
+	return {
+		status: 200,
+		data: {
+			user: userData
+		}
+	};
+};
+
+export const actions: Actions = {
+	update_profile_data: async ({ request, cookies }) => {
+		const formData = await request.formData();
+
+		const userUpdate = z
+			.object({
+				vorname: z.string().min(3),
+				nachname: z.string().min(3),
+				hausnr: z.string().min(1),
+				plz: z.string().min(5).max(5),
+				ort: z.string().min(1).max(50),
+				strasse: z.string().min(2),
+				telefon: z.string().min(2)
+			})
+			.safeParse(Object.fromEntries(formData));
+
+		if (!userUpdate.success)
+			return fail(400, {
+				error: 'Deine Accountdaten wurden nicht aktualisiert.'
+			});
+
+		let user;
+		const token = cookies.get('token');
+		if (!token || !process.env.JWT_SECRET) {
+			return fail(400, {
+				error: 'Deine Accountdaten wurden nicht aktualisiert.'
+			});
+		}
+		try {
+			user = jwt.verify(token, process.env.JWT_SECRET) as Prisma.UserCreateInput;
+		} catch (error) {
+			console.error(error);
+		}
+
+		if (!user) {
+			return fail(400, {
+				error: 'Deine Accountdaten wurden nicht aktualisiert.'
+			});
+		}
+
+		const updatedUser = await prisma.user.update({
+			where: {
+				id: user.id
+			},
+			data: {
+				vorname: userUpdate.data.vorname,
+				nachname: userUpdate.data.nachname,
+				hausnr: userUpdate.data.hausnr,
+				plz: userUpdate.data.plz,
+				ort: userUpdate.data.ort,
+				strasse: userUpdate.data.strasse,
+				telefon: userUpdate.data.telefon
+			}
+		});
+
+		if (!updatedUser) {
+			return fail(400, {
+				error: 'Deine Accountdaten wurden nicht aktualisiert.'
+			});
+		}
+
+		try {
+			const newToken = jwt.sign(updatedUser, process.env.JWT_SECRET);
+			cookies.set('token', newToken);
+		} catch (error) {
+			console.error(error);
+		}
+
+		return {
+			status: 200,
+			user: updatedUser
+		};
+	},
+	update_password: async ({ request, cookies }) => {
+		const formData = await request.formData();
+
+		const password = z
+			.object({
+				password: z.string().min(8)
+			})
+			.safeParse(Object.fromEntries(formData));
+
+		if (!password.success)
+			return fail(400, {
+				error: 'Deine Accountdaten wurden nicht aktualisiert.'
+			});
+
+		const newPassword = await Bun.password.hash(password.data.password, {
+			algorithm: 'bcrypt',
+			cost: 10
+		});
+
+		let user;
+		const token = cookies.get('token');
+		if (!token || !process.env.JWT_SECRET) {
+			return fail(400, {
+				error: 'Deine Accountdaten wurden nicht aktualisiert.'
+			});
+		}
+		try {
+			user = jwt.verify(token, process.env.JWT_SECRET) as Prisma.UserCreateInput;
+		} catch (error) {
+			console.error(error);
+		}
+
+		if (!user) {
+			return fail(400, {
+				error: 'Deine Accountdaten wurden nicht aktualisiert.'
+			});
+		}
+
+		const updatedUser = await prisma.user.update({
+			where: {
+				id: user.id
+			},
+			data: {
+				password: newPassword
+			}
+		});
+
+		if (!updatedUser) {
+			return fail(400, {
+				error: 'Deine Accountdaten wurden nicht aktualisiert.'
+			});
+		}
+
+		return {
+			status: 200,
+			user: updatedUser
+		};
+	}
+};
