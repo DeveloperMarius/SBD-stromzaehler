2d748fdfec8d645ec76d2606709c4b811044e3a6
jakscha <mails-straeucher0l@icloud.com>
2023-12-05 17:42:36 +0100

    Implemented entity check in auth_middleware to enable least privilege access

--- smart-meter-messstellenbetreiber/src/auth_middleware.py
+++ smart-meter-messstellenbetreiber/src/auth_middleware.py
@@ -1,28 +1,30 @@
 from functools import wraps
 from flask import request
-from utils import get_jwt_from_request, is_jwt_in_request, is_body_signature_valid
+from utils import get_jwt_from_request, is_jwt_in_request
 from utils import Variables
 
 
-def token_required(f):
+def token_required(entity_type):
+    def decorator(f):
 
-    @wraps(f)
-    def decorated(*args, **kwargs):
+        @wraps(f)
+        def decorated(*args, **kwargs):
 
-        jwt = get_jwt_from_request(request)
-        if jwt is None:
-            if is_jwt_in_request(request):
-                message = 'Invalid Authentication Token'
-            else:
-                message = 'Authentication Token not provided'
-            Variables.get_logger().log(request, message)
-            return {
-                "message": message,
-                "data": None,
-                "error": "Unauthorized"
-            }, 401
+            jwt = get_jwt_from_request(request, entity_type)
+            if jwt is None:
+                if is_jwt_in_request(request):
+                    message = 'Invalid Authentication Token'
+                else:
+                    message = 'Authentication Token not provided'
+                Variables.get_logger().log(request, message)
+                return {
+                    "message": message,
+                    "data": None,
+                    "error": "Unauthorized"
+                }, 401
 
-        Variables.get_logger().log(request, 'Successfully authorized!')
-        return f(jwt['id'], *args, **kwargs)
+            Variables.get_logger().log(request, 'Successfully authorized!')
+            return f(jwt['id'], *args, **kwargs)
 
-    return decorated
+        return decorated
+    return decorator
--- smart-meter-messstellenbetreiber/src/utils.py
+++ smart-meter-messstellenbetreiber/src/utils.py
@@ -3,7 +3,6 @@ import jwt
 import os
 import time
 from models import Stromzaehler, Log, Kundenportal
-from datetime import datetime
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sqlalchemy import select
@@ -14,8 +13,10 @@ import hashlib
 import sys
 import traceback
 
+
 def get_current_milliseconds():
     return round(time.time() * 1000)
+
 
 class Variables:
     db_instance = None
@@ -73,7 +74,7 @@ def is_body_signature_valid(request, jwt_body) -> bool:
     return actual_hash == jwt_body['signature']
 
 
-def get_jwt_from_request(request):
+def get_jwt_from_request(request, entity_type):
     if not is_jwt_in_request(request):
         return None
 
@@ -83,15 +84,21 @@ def get_jwt_from_request(request):
     if jwt_data is None or jwt_data['type'] is None or jwt_data['id'] is None:
         return None
 
-    if jwt_data['type'] == 'stromzaehler':
-        statement = select(Stromzaehler.public_key).where(Stromzaehler.id == jwt_data['id'])
+    if entity_type == 'stromzaehler':
+        if jwt_data['type'] == 'stromzaehler':
+            statement = select(Stromzaehler.public_key).where(Stromzaehler.id == jwt_data['id'])
 
-        with Session(Variables.get_database().get_engin()) as session:
-            public_key = session.scalar(statement).replace('\\n', '\n')
-    elif jwt_data['type'] == 'kundenportal':
-        statement = select(Kundenportal.public_key).where(Kundenportal.id == jwt_data['id'])
-        with Session(Variables.get_database().get_engin()) as session:
-            public_key = session.scalar(statement).replace('\\n', '\n')
+            with Session(Variables.get_database().get_engin()) as session:
+                public_key = session.scalar(statement).replace('\\n', '\n')
+        else:
+            return None
+    elif entity_type == 'kundenportal':
+        if jwt_data['type'] == 'kundenportal':
+            statement = select(Kundenportal.public_key).where(Kundenportal.id == jwt_data['id'])
+            with Session(Variables.get_database().get_engin()) as session:
+                public_key = session.scalar(statement).replace('\\n', '\n')
+        else:
+            return None
     else:
         return None
 
